85f25e9632396de7e0c9f25b3575c4256d36d9df
Hugo Valente <hugo.valente1996@gmail.com>
2018-07-16 08:47:04 +0200

    wip

--- init.lua
+++ init.lua
@@ -1,1 +1,15 @@
-print("\nStart hg180713.0926\n")
+-- Programme qui fait quelque chose quand on appuie sur le btn flash
+-- hv180713.1543
+
+zswitch=3     --switch flash
+gpio.mode(zswitch, gpio.INT, gpio.PULLUP)
+
+function bouton()
+    print("Start robot_1.lua...")
+    dofile("robot_1.lua")
+end
+
+gpio.trig(zswitch, "both", bouton)
+
+print("\nhvinit 180713.1544\n")
+
