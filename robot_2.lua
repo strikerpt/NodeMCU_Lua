a40c92aab9e7b3be0f68fc366ca5fe27265bd821
strikerpt <39523323+strikerpt@users.noreply.github.com>
2018-07-17 15:08:45 +0200

    Update robot_2.lua

--- robot_2.lua
+++ robot_2.lua
@@ -1,4 +1,4 @@
---Programme pour faire tourner le robot à droite tant qu'il soit à moins de 20cm de l'obstacle
+--Programme pour faire tourner le robot à droite ou à gauche tant qu'il soit à moins de 20cm de l'obstacle
 --hv20180713.1551
 
 --parametres pour le module ultra son 
@@ -9,6 +9,7 @@ ztstop=0
 gpio.mode(ztrig, gpio.OUTPUT)
 gpio.write(ztrig, gpio.LOW)
 gpio.mode(zecho, gpio.INT, gpio.PULLUP)
+
 --parametres pour les moteurs
 pin_a_speed = 1
 pin_a_dir = 3
@@ -17,6 +18,7 @@ pin_b_dir = 4
 FWD = gpio.LOW
 REV = gpio.HIGH
 duty = 1023
+
 --initialise moteur A
 gpio.mode(pin_a_speed,gpio.OUTPUT)
 gpio.write(pin_a_speed,gpio.LOW)
@@ -24,6 +26,7 @@ pwm.setup(pin_a_speed,1000,duty) --PWM 1KHz, Duty 1023
 pwm.start(pin_a_speed)
 pwm.setduty(pin_a_speed,0)
 gpio.mode(pin_a_dir,gpio.OUTPUT)
+
 --initialise moteur B
 gpio.mode(pin_b_speed,gpio.OUTPUT)
 gpio.write(pin_b_speed,gpio.LOW)
@@ -31,6 +34,7 @@ pwm.setup(pin_b_speed,1000,duty) --PWM 1KHz, Duty 1023
 pwm.start(pin_b_speed)
 pwm.setduty(pin_b_speed,0)
 gpio.mode(pin_b_dir,gpio.OUTPUT)
+
 -- speed is 0 - 100
 function motor(pin_speed, pin_dir, dir, speed)
     gpio.write(pin_dir,dir)
@@ -44,16 +48,15 @@ end
 function motor_b(dir, speed)
     motor(pin_b_speed, pin_b_dir, dir, speed)
 end
+
 --start pulse10 us
 function zmesure_pulse()
     gpio.write(ztrig, gpio.HIGH)
     tmr.delay(10)
     gpio.write(ztrig, gpio.LOW)
 end
+
 -- mesure la distance et stop si < 20cm
-
-
-
 function zmesure()
     if gpio.read(zecho)==1 then 
         ztstart=tmr.now()
@@ -63,44 +66,44 @@ function zmesure()
         if zlength>200 then zlength=0 end  
         print("distance [cm]: "..math.floor(zlength))
         if zlength<20 then 
-            print("tourne connard")
             if t==1 then 
                 turn_left_robot()
             else 
                 turn_right_robot()
             end
-            tmr.alarm(2, 500, tmr.ALARM_SINGLE, avance_robot)
---            tmr.stop(1)
---            motor_a(FWD, 0)
---            motor_b(FWD, 0)
+            tmr.alarm(2, 500, tmr.ALARM_SINGLE, avance_robot) -- Pour qu'il arrête de tourner 
         end
     end
 end
 
-
-
-
 gpio.trig(zecho, "both", zmesure)
-tmr.alarm(1, 200, tmr.ALARM_AUTO, zmesure_pulse)
+tmr.alarm(1, 200, tmr.ALARM_AUTO, zmesure_pulse) -- à chaque 200 ms il mesure la distance
+
 --Robot avance
 function avance_robot()
-    print("avance")
+    --Pour qu'il tourne à gauche ou à droite de façon random
     t=math.random(1,2)
     print(t)
     motor_a(FWD, 60)
     motor_b(FWD, 60)
 end
+
+--Robot s'arrete
 function stop_robot()
     motor_a(FWD, 0)
     motor_b(FWD, 0)
 end
 
+--Robot tourne à droite
 function turn_right_robot()
     motor_a(FWD, 60)
     motor_b(REV, 60)
 end
+
+--Robot tourne à gauche
 function turn_left_robot()
     motor_a(REV, 60)
     motor_b(FWD, 60)
 end
+
 avance_robot()
