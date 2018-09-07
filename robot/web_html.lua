6025f87bea6ab4646d51836697c2cced8d37e95e
Hugo Valente <hugo.valente1996@gmail.com>
2018-09-07 16:25:01 +0200

    - améliorer les scripts et débugé quelques erreurs

--- robot/web_html.lua
+++ robot/web_html.lua
@@ -1,6 +1,6 @@
 -- petit script pour le HTML du serveur web
 
-print("\n web_html.lua   hv180907.1440   \n")
+print("\n web_html.lua   hv180907.1542  \n")
 
 --Partie HTML et CSS pour la page web
 function web_html()  
@@ -16,7 +16,6 @@ function web_html()
     buf = buf .. "<h1> Tests : </h1>\n" 
     buf = buf .. "<a href='?pin=T1'><button id='bt1'>Start</button></a><a href='?pin=T2'><button id='bt2'>Reboot</button></a>"
     buf = buf .. "<a href='?pin=T3'><button id='bt3'>T3</button></a><a href='?pin=T4'><button id='bt4'>T4</button></a>\n"
-    buf = buf .. "<h1> Wifi : </h1>\n" 
     buf = buf .. "<style>\n"
     buf = buf .. "#bf, #bb, #bl, #br, #bs, #bsl, #bsm, #bsf, #ba, #bml, #bt1, #bt2, #bt3, #bt4{font-size:10px; position:relative; height:50px; width:50px;}\n"
     buf = buf .. "#bf, #bb {left:50px;}\n"   
