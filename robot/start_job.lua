0c1bb9f2b44d27e1541f478c458f4bffae505aba
Hugo Valente <hugo.valente1996@gmail.com>
2018-09-07 10:15:17 +0200

    - modifié le code start_job.lua pour qu'il déclanche le timer seulement s'il est en auto et s'il est à plus de 20 cm de l'obstacle - ajouté un troisième bouton pour afficher les mesures sur le display quand il est en mode manuel

--- robot/start_job.lua
+++ robot/start_job.lua
@@ -1,4 +1,4 @@
-print("\n start_job.lua hv180906.1648 \n")
+print("\n start_job.lua hv180907.0928 \n")
 
 jobtimer1=tmr.create()
 
@@ -19,42 +19,34 @@ disp_oled()
 zpeed=50
 zauto=true
 
-function mesurewhenmanual()
-    if zauto then 
-        tmr.stop(jobtimer1)
-    else 
-        tmr.alarm(jobtimer1, 600, tmr.ALARM_AUTO, start_mesure)
-    end
-end
-
 function return_mesure()
     print(zlength)
     print("RAM: "..node.heap())
-
- if zauto then 
-        if zlength<0.20 then
-          t=math.random(1,2)
-          if t==1 then
-            right()
-          else
-            left()
-          end 
+    
+    if zauto then 
+        if zlength < 0.20 then
+            if math.random(1,2) == 1 then
+                right()
+            else
+                left()
+            end 
         end
+        tmr.alarm(jobtimer1, 200, tmr.ALARM_SINGLE, start_mesure)
     else
-        oled_line1=zlength.." m"
-        oled_line2=""
-        oled_line3=""
-        oled_line4="NodeMCU: "..wifi.ap.getmac()        
-        disp_oled()
+        if zmeter then
+            oled_line1=zlength.." m"
+            oled_line2=""
+            oled_line3=""
+            oled_line4="NodeMCU: "..wifi.ap.getmac()        
+            disp_oled()
+        end
     end
 end
 
---dofile("motor.lua")
 dofile("detector.lua")
-
-mesurewhenmanual()
+tmr.alarm(jobtimer1, 200, tmr.ALARM_SINGLE, start_mesure)
 forward()
---tmr.alarm(jobtimer1, 600, tmr.ALARM_AUTO, start_mesure)
+
 
 
 
